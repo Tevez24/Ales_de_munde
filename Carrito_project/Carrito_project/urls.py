359631a6992e1378af68a32d344b5021698e730b
roobertojules <roobertojules@gmail.com>
2025-10-29 20:26:32 +0000

    Add debug message to login view

--- Carrito_project/Carrito_project/urls.py
+++ Carrito_project/Carrito_project/urls.py
@@ -1,21 +1,13 @@
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
-
-
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Carrito_app.urls')),
-    # Rutas para JWT
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('Carrito_app.urls')),  # Incluir las URLs de la app
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
